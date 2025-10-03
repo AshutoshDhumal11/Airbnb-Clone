6f9ca364d0510cf847354de4b2fbec55858535ba
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-10-03 19:00:59 +0530

    Added listing page and related features

---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,22 @@
+import { getListingById } from "@/app/actions/getListingById";
+import { EmptyState } from "@/app/components/EmptyState";
+import { ListingClient } from "./ListingClient";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { getReservations } from "@/app/actions/getReservations";
+
+interface IParams {
+  listingId: string;
+}
+
+export default async function Listing({ params }: { params: IParams }) {
+  const listing = await getListingById(params);
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return <EmptyState />;
+  }
+  return <div className="">
+    <ListingClient listing={listing} reservations={reservations} currentUser={currentUser}/>
+  </div>;
+}
