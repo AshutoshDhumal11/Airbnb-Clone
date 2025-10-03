a524f3ee8d052a95e75dbd23d12eea877de58e1e
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-10-03 23:34:04 +0530

    Updated authOptions file

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -49,7 +49,9 @@ export const authOptions: AuthOptions = {
         })
     ],
     pages: {
-        signIn: "/"
+        signIn: "/",
+        signOut: "/",
+        error: "/"
     },
     debug: process.env.NODE_ENV === "development",
     session: {
